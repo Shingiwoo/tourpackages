b8addad6127ae08349494ff09dcfc57fec5e3b11
shingiwoo <shingiwoo.ind@gmail.com>
2024-11-20 22:01:00 +0700

    first commit

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+
+            //Admin
+            [
+                'name'      => 'Shin Sugiono',
+                'username'  => 'admin',
+                'email'     => 'admin@gmail.com',
+                'password'  => Hash::make('admin1234'),
+                'role'      => 'admin',
+                'status'      => 'active',
+            ],
+
+            //Agen
+            [
+                'name'      => 'Agen Travel',
+                'username'  => 'agen',
+                'email'     => 'agen@gmail.com',
+                'password'  => Hash::make('agen1234'),
+                'role'      => 'agen',
+                'status'      => 'active',
+            ],
+
+        ]);
+    }
+}
---
+++ resources/views/admin/body/sidebar.blade.php
@@ -0,0 +1,67 @@
+<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
+    <div class="app-brand demo">
+        <a href="index.html" class="app-brand-link">
+            <span class="app-brand-logo demo">
+
+            </span>
+            <span class="app-brand-text demo menu-text fw-bold">Paket Wisata</span>
+        </a>
+
+        <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto">
+            <i class="ti menu-toggle-icon d-none d-xl-block align-middle"></i>
+            <i class="ti ti-x d-block d-xl-none ti-md align-middle"></i>
+        </a>
+    </div>
+
+    <div class="menu-inner-shadow"></div>
+
+    <ul class="menu-inner py-1">
+        <!-- Dashboards -->
+        <li class="menu-item">
+            <a class="menu-link">
+                <i class="menu-icon tf-icons ti ti-layout-dashboard"></i>
+                <div data-i18n="Dashboards">Dashboards</div>
+            </a>
+        </li>
+
+        <!-- Apps & Pages -->
+        <li class="menu-header small">
+            <span class="menu-header-text" data-i18n="Apps & Pages">Apps &amp; Pages</span>
+        </li>
+        <li class="menu-item">
+            <a href="app-email.html" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-mail"></i>
+                <div data-i18n="Email">Email</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="app-chat.html" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-messages"></i>
+                <div data-i18n="Chat">Chat</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="app-calendar.html" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-calendar"></i>
+                <div data-i18n="Calendar">Calendar</div>
+            </a>
+        </li>
+        <li class="menu-item">
+            <a href="app-kanban.html" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-layout-kanban"></i>
+                <div data-i18n="Kanban">Kanban</div>
+            </a>
+        </li>
+
+        <!-- Misc -->
+        <li class="menu-header small">
+            <span class="menu-header-text" data-i18n="Misc">Misc</span>
+        </li>
+        <li class="menu-item">
+            <a href="https://pixinvent.ticksy.com/" target="_blank" class="menu-link">
+                <i class="menu-icon tf-icons ti ti-lifebuoy"></i>
+                <div data-i18n="Support">Support</div>
+            </a>
+        </li>
+    </ul>
+</aside>
